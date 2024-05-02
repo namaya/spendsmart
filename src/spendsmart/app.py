d779acf5b67eebba8b8a3e375b1e49990acc02db
namaya <n3amaya@gmail.com>
2024-05-02 07:02:05 -0500

    add errctx utility function

--- src/spendsmart/app.py
+++ src/spendsmart/app.py
@@ -1,5 +1,28 @@
+import csv
+import logging
+from pathlib import Path
+import sys
+
+from spendsmart.utils import errctx
+
+
+logger = logging.getLogger(__name__)
+
+
 def main():
-    print("Hello, World!")
+    if len(sys.argv) >= 2:
+        index(Path(sys.argv[1]))
+
+
+@errctx("Couldn't index file '{file}'.")
+def index(file: Path):
+    if not file.exists():
+        raise FileNotFoundError(f"File not found.")
+
+    if not file.suffix == ".csv":
+        raise ValueError(f"File format is not supported.")
+
+    logger.info(f"Indexing file '{file}'.")
 
 
 if __name__ == "__main__":
---
+++ src/spendsmart/utils.py
@@ -0,0 +1,27 @@
+import functools
+import inspect
+
+
+def errctx(ctx: str):
+    """Add context to raised exceptions."""
+
+    def decorator_errctx(func):
+        @functools.wraps(func)
+        def errctx_wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                signature = inspect.signature(func)
+                bound_args = signature.bind(*args, **kwargs)
+
+                bound_args.apply_defaults()
+
+                merged_ctx = ctx.format(**bound_args.arguments)
+
+                raise (type(e))(f"{merged_ctx} {str(e)}").with_traceback(
+                    e.__traceback__
+                )
+
+        return errctx_wrapper
+
+    return decorator_errctx
