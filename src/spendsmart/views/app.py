cbb939b96c3ee543873118b109893261e7d5c551
namaya <n3amaya@gmail.com>
2024-05-16 21:13:49 -0500

    add some vim bindings to views

--- src/spendsmart/views/app.py
+++ src/spendsmart/views/app.py
@@ -1,7 +1,16 @@
+from textual import events
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, OptionList
+from textual.widgets import Header, Footer, OptionList, RichLog
 
 from spendsmart.controllers import TxnController
+
+
+class VimOptionList(OptionList):
+    def on_key(self, event: events.Key) -> None:
+        if event.name == "j":
+            self.action_cursor_down()
+        elif event.name == "k":
+            self.action_cursor_up()
 
 
 class SpendSmartApp(App):
@@ -11,7 +20,12 @@ class SpendSmartApp(App):
         self._txncontrol = txncontrol
 
     def compose(self) -> ComposeResult:
-        txns = self._txncontrol.list(10)
+        txns = self._txncontrol.fetch_txns(10)
+
         yield Header()
-        yield OptionList(*[txn.description for txn in txns])
+        yield VimOptionList(*[txn.description for txn in txns])
+        yield RichLog()
         yield Footer()
+
+    def on_key(self, event: events.Key) -> None:
+        self.query_one(RichLog).write(event)
